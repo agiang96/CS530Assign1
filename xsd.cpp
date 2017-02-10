da1cfdb385b5833c89599d3187eb88a58faefc6e
Generic Account 0885 <cssc0885@edoras.sdsu.edu>
2017-02-09 16:19:06 -0800

    reformatted xsd.cpp into functions

--- xsd.cpp
+++ xsd.cpp
@@ -15,72 +15,51 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
+void hexDump(char file[50]){
     int address = 0;
-    char file[50];
     string line_;
-    cout << "Name a file ";
-    cin >> file;
     unsigned char ch; //to get each line char by char
-    
-    cout << "Do you want it to output in binary or hex?" << endl;
-    cout << "Choose 1 for binary and 2 for hex" << endl;
-    
-    int choose = 0;
-    while(choose < 1 || choose > 2){
-        cin >> choose;
-        if (choose < 1 || choose > 2){
-            cout << "Not the number. Choose again: ";
-        }
-    }
+
     ifstream file_(file);
     if(file_.is_open()){
 
 
-    cout << hex << setfill('0');
+        cout << hex << setfill('0');
 
 	// until the end of file
         while(file_.good()){
             int j = 0;
-
+	    int x = 0;
 		//prints out 16 bytes
             for(j = 0; j < 16 && file_ >> noskipws >> ch ; j++ ){
                 if (j == 0){
-                    cout << setw(7) << address << ": ";
+                    cout << setw(7) << address << ":";
                 }
-                if(ch < 32){
-                    ch = '.';
+                if(j % 2 == 0){
+                   cout << " ";
                 }
-                if(choose == 1){
-                    cout<< bitset<8>(ch) <<" ";
-                }
-                if(choose == 2){
-                    if(j % 2 == 0){
-                        cout << " ";
-                    }
-                    cout<< hex << setw(2) << int(ch);
-                }
+                cout<< hex << setw(2) << int(ch);
                 
                 line_[j] = ch;
             }
-            
+            x = j; //won't pring end NULL's
             //for organization
             while(j < 16){
-                if(choose == 1){
-                    cout << "         ";
-                }
-                if(choose == 2){
-                    cout << "   ";
-                }
+                cout << "  ";
+                if(j % 2 == 0)
+                cout << " ";
                 
                 j++;
             }
             
             //to print out the text
             cout << " ";
-            for(int i = 0; i < 16; i++){
+            for(int i = 0; i < x; i++){
                 
-                cout << line_[i];
+		if(line_[i] < 32)
+		    cout << ".";
+		else
+                    cout << line_[i];
                 
             }
             for(int i = 0; i < 16; i++){
@@ -94,10 +73,93 @@ int main(int argc, char** argv) {
         file_.close();
     }
     else
-        cout<<"file is not open"<<'\n';
-    	cin.get();
-    
-    return 0;
+        cout<<"file is not open"<<'\n';    
 }
+    
+void binDump(char file[50]){
+    int address = 0;
+    string line_;
+    unsigned char ch; //to get each line char by char
+
+    ifstream file_(file);
+    if(file_.is_open()){
+
+
+        cout << hex << setfill('0');
+
+	// until the end of file
+        while(file_.good()){
+            int j = 0;
+	    int x = 0;
+		//prints out 6 bytes
+            for(j = 0; j < 6 && file_ >> noskipws >> ch ; j++ ){
+                if (j == 0){
+                    cout << setw(7) << address << ": ";
+                }
+
+                cout<< bitset<8>(ch) <<" ";
+                
+                line_[j] = ch;
+            }
+            x = j; //won't pring end NULL's
+
+            //for organization
+            while(j < 6){
+                cout << "         ";
+                
+                j++;
+            }
+            
+            //to print out the text
+            cout << " ";
+            for(int i = 0; i < x; i++){
+                
+		if(line_[i] < 32)
+		    cout << ".";
+		else
+                    cout << line_[i];
+                
+            }
+            for(int i = 0; i < 6; i++){
+                line_[i] = NULL;
+            }
+            cout << endl;
+            
+            address += 6;
+        }
+        
+        file_.close();
+    }
+    else
+        cout<<"file is not open"<<'\n';    
+}
+
+
+int main(int argc, char** argv) {
+    char file[50];
+    cout << "Name a file ";
+    cin >> file;
+    
+    cout << "Do you want it to output in binary or hex?" << endl;
+    cout << "Choose 1 for binary and 2 for hex" << endl;
+    
+    int choose = 0;
+    while(choose < 1 || choose > 2){
+        cin >> choose;
+        if (choose < 1 || choose > 2){
+            cout << "Not the number. Choose again: ";
+        }
+    }
+
+    if(choose == 2) {
+        hexDump(file);
+    }
+
+    if(choose ==1) {
+        binDump(file);
+    }
+
+}
+
 
 
